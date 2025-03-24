1eab4ae34a2a564dfebed8fa5d18151779bd80e6
GabrielCanto-git <gabrielcanto.ap@gmail.com>
2025-03-24 16:25:20 -0300

    Update testebancodedados.py

--- site-exemplo/testebancodedados.py
+++ site-exemplo/testebancodedados.py
@@ -9,12 +9,13 @@ dados_conexao = (
 conexao = pyodbc.connect(dados_conexao)
 print("Conexão Bem Sucedida")
 
-cursor = conexao.cursor()
-comando = """INSERT INTO  Usuarios(id, nome, email, telefone)
-VALUES 
+cursor = conexao.cursor()  #É um elemento que vai executar os comandos em sql no banco de dados
+
+comando = """INSERT INTO Usuarios(id, nome, email, telefone)
+VALUES
 	(2, 'roberto', 'roberto@hotmail.com', 232)"""
 
-cursor.execute(comando)
-cursor.commit()
+cursor.execute(comando)#comando que executa
+cursor.commit()#Só precisa disso caso o comando edite o banco de dados
 
-
+#sd
