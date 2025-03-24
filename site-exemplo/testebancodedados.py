379a7118c42039d18940fbca9c2dd7a7dc48cfdb
GabrielCanto-git <gabrielcanto.ap@gmail.com>
2025-03-24 01:23:24 -0300

    Teste do banco de dados
    testando uma forma de conectar o banco dados com python

--- site-exemplo/testebancodedados.py
+++ site-exemplo/testebancodedados.py
@@ -2,18 +2,18 @@ import pyodbc
 
 dados_conexao = (
     "Driver={SQL Server};"
-    "Server=localhost\SQLEXPRESS01;Database=master;Trusted_Connection=True;"
-    "Database=SistemaBiblioteca;"
+    "Server=Note_do_Gabs\SQLEXPRESS01;"
+    "Database=PythonSQL;"
 )
 
 conexao = pyodbc.connect(dados_conexao)
 print("Conexão Bem Sucedida")
 
 cursor = conexao.cursor()
-
 comando = """INSERT INTO  Usuarios(id, nome, email, telefone)
 VALUES 
-	(1, 'Roberto', 'roberto@hotmail.com', 249)"""
+	(2, 'roberto', 'roberto@hotmail.com', 232)"""
 
 cursor.execute(comando)
 cursor.commit()
+
