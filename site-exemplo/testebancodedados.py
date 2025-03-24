62b4aed42c20d2736c2f9e38f66530af3244ddea
GabrielCanto-git <gabrielcanto.ap@gmail.com>
2025-03-24 00:50:04 -0300

    Update testebancodedados.py

--- site-exemplo/testebancodedados.py
+++ site-exemplo/testebancodedados.py
@@ -10,3 +10,10 @@ conexao = pyodbc.connect(dados_conexao)
 print("Conexão Bem Sucedida")
 
 cursor = conexao.cursor()
+
+comando = """INSERT INTO  Usuarios(id, nome, email, telefone)
+VALUES 
+	(1, 'Roberto', 'roberto@hotmail.com', 249)"""
+
+cursor.execute(comando)
+cursor.commit()
